cbe81360079fe6aa1f20c5881d951553f78174b4
UnclElmino <msiemilio@myseneca.ca>
2025-03-06 23:00:17 -0500

    Commit 1

---
+++ server.js
@@ -0,0 +1,36 @@
+// Get the button
+let mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+mybutton.addEventListener("click", topFunction);
+
+document.getElementById("LinkedIn").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/emilioimam/", "_blank");
+});
+
+document.getElementById("Github").addEventListener("click", function() {
+    window.open("https://github.com/UnclElmino/Portfolio", "_blank");
+});
+
+document.getElementById("Email").addEventListener("click", function() {
+    window.location.href = "mailto:your-email@example.com";
+});
+
+document.getElementById("Resume").addEventListener("click", function() {
+    window.open("EmilioShakhawat_resume.pdf", "_blank");
+});
