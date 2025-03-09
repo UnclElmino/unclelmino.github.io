ab31e6ba00b5fd51e2c6f3a7bdc3f511c3fa6224
UnclElmino <msiemilio@myseneca.ca>
2025-03-09 17:56:48 -0400

    updates

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ document.getElementById("Resume").addEventListener("click", function() {
 
 // Open link when sysDiagnose card is clicked
 document.getElementById("sysDiagnose").addEventListener("click", function() {
-    window.open("https://github.com/UnclElmino/Portfolio/tree/e35a16665bcc4b27a764194eee22d2e25c773e9b/System%20diagnosis%20tool", "_blank");
+    window.open("https://github.com/UnclElmino/Portfolio/tree/main/System%20diagnosis%20tool", "_blank");
 });
